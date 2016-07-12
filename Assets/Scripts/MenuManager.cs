aacf67300a3ad158371f7d128c169746a010e3b7
Thierry Berger <tom.kahl@hotmail.fr>
2016-07-12 21:09:51 +0200

    add unity project

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    public void StartGame()
+    {
+        Debug.Log("start!");
+        //Application.LoadLevel("RocketMouse");
+    }
+}
